498d871d35213cb88ffd96d4b5727c6d4576e307
ATran2000 <89952087+ATran2000@users.noreply.github.com>
2024-01-31 17:45:29 -0500

    I learned how to create a friend system for a Django application.
    I learned how to create a friend system in a Django application. This code contains user creation, authentication, and a friend system where users are able to see, send, accept, and remove friend requests.

---
+++ AddingFriends/README.md
@@ -0,0 +1,4 @@
+# About
+
+I learned how to create a friend system in a Django application. This code contains user creation, authentication, and a friend system where users are 
+able to see, send, accept, and remove friend requests
---
+++ AddingFriends/api_user/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import User, Friendship
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'username', 'is_staff', 'is_active', 'date_joined')
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Friendship)
